d525c633f3f86f9432b9a03a15d0239eda4178c8
Rakesh Kumar <webmkumar@gmail.com>
2024-04-28 14:30:17 +0530

    2nd commit

---
+++ src/main/java/com/blog13/controller/PaymentController.java
@@ -0,0 +1,9 @@
+package com.blog13.controller;
+
+public class PaymentController {
+    private String paymentType;
+
+    public PaymentController(String paymentType) {
+        this.paymentType = paymentType;
+    }
+}
